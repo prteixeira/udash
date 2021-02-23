d0a6c1043604c14cf064777a0dbdf1953c314434
Paulo Ricardo Teixeira <paulo.ricardo.teixeira@gmail.com>
2021-02-23 17:49:53 -0400

    Create app.py
    add app.py for test

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import yaml
+import numpy as np
+import pandas as pd
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
